54b910096a9a35a6cc069acad30d00dd19cab590
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-15 16:20:57 -0700

    Resolved conflicts and created trip controller and corssponding view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
-  get 'drivers/index'
-  get 'passengers/index'
->>>>>>> dc6014c67d31303f4678016d083ad5fba1eb94b3
+  # get 'trips/index'
+  # get 'drivers/index'
+  # get 'passengers/index'
+
+  resources :drivers, :passengers, :trips
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
