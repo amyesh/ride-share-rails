dc6014c67d31303f4678016d083ad5fba1eb94b3
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-04-15 15:37:54 -0700

    Initial commit with controllers for Drivers and Passengers

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'drivers/index'
+  get 'passengers/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
