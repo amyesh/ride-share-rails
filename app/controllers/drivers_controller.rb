f8e584fa52e493cde8a0a5ed3576ae2418cdfed0
amyesh <amyesh08@gmail.com>
2019-04-16 15:19:31 -0700

    added view pages for all basic actions

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -3,7 +3,6 @@ class DriversController < ApplicationController
     @drivers = Driver.all.order(:id)
   end
 
-<<<<<<< HEAD
   def show
     driver_id = params[:id]
     @driver = Driver.find_by(id: driver_id)
@@ -11,8 +10,8 @@ class DriversController < ApplicationController
       head :not_found
     end
   end
-=======
-  def new 
+
+  def new
     @driver = Driver.new(name: "John Doe", vin: "123456789ABCDEFGH")
   end
 
@@ -28,10 +27,9 @@ class DriversController < ApplicationController
     end
   end
 
-  private 
+  private
 
   def driver_params
     return params.require(:driver).permit(:name, :vin)
   end
->>>>>>> ef3af17f1456765f71d3da975f681e8b875a9d36
 end
