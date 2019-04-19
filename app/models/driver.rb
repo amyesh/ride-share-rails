0a9de26eb37cbef603053544655de3ba482bf67a
amyesh <amyesh08@gmail.com>
2019-04-19 15:10:13 -0700

    fixed make model error

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,8 +2,6 @@ class Driver < ApplicationRecord
   has_many :trips
 
   validates :name, presence: true
-  validates :car_make, presence: true
-  validates :car_model, presence: true
   validates :vin, presence: true, uniqueness: true
 
   def average_rating
