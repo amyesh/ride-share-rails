74c865fd615b745d06294e328247d858679fd9f4
amyesh <amyesh08@gmail.com>
2019-04-18 18:14:22 -0700

    added edit test for trips controller

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -32,7 +32,13 @@ describe TripsController do
   end
 
   describe "edit" do
-    # Your tests go here
+    it "can get edit path for trip" do
+      driver = Driver.create(name: "Amy Martinsen", vin: "ABCDEFG")
+      passenger = Passenger.create(name: "Faiza Ahsan", phone_num: "555-555-5555")
+      trip = Trip.create(passenger_id: passenger.id, driver_id: driver.id, rating: 5, cost: 1474, date: "2016-06-02")
+      get edit_trip_path(trip.id)
+      must_respond_with :success
+    end
   end
 
   describe "update" do
@@ -47,3 +53,61 @@ describe TripsController do
     # Your tests go here
   end
 end
+
+# describe "update" do
+#   it "will update an existing passenger" do
+#     starter_input = {
+#       name: "Faiza Ahsad",
+#       phone_num: "555-555-5555",
+#     }
+
+#     passenger_to_update = Passenger.create(starter_input)
+
+#     input_name = "Amy Martinsen"
+#     input_phone_num = "222-222-2222"
+#     test_input = {
+#       "passenger": {
+#         name: input_name,
+#         phone_num: input_phone_num,
+#       },
+#     }
+
+#     expect {
+#       patch passenger_path(passenger_to_update.id), params: test_input
+#     }.wont_change "Passenger.count"
+
+#     must_respond_with :redirect
+#     passenger_to_update.reload
+#     expect(passenger_to_update.name).must_equal test_input[:passenger][:name]
+#     expect(passenger_to_update.phone_num).must_equal test_input[:passenger][:phone_num]
+#   end
+
+#   it "will return a bad_request (400) when asked to update with invalid data" do
+#     starter_input = {
+#       name: "Punchinello Funny Fellow",
+#       phone_num: "867-5309",
+#     }
+
+#     passenger_to_update = Passenger.create(starter_input)
+
+#     input_name = ""
+#     input_phone_num = "123-456-7890"
+#     test_input = {
+#       "passenger": {
+#         name: input_name,
+#         phone_num: input_phone_num,
+#       },
+#     }
+
+#     # Act
+#     expect {
+#       patch passenger_path(passenger_to_update.id), params: test_input
+#     }.wont_change "Passenger.count"
+
+#     # Assert
+#     must_respond_with :bad_request
+#     passenger_to_update.reload
+#     expect(passenger_to_update.name).must_equal starter_input[:name]
+#     expect(passenger_to_update.phone_num).must_equal starter_input[:phone_num]
+#   end
+# end
