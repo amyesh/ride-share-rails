6dd1b299bcc90c891d417239210367e58696d6d5
amyesh <amyesh08@gmail.com>
2019-04-19 13:24:24 -0700

    fixed merge conflict

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -95,7 +95,6 @@ describe PassengersController do
   end
 
   describe "new" do
-<<<<<<< HEAD
     it "can get new path for passenger" do
       get new_passenger_path
       must_respond_with :success
@@ -103,8 +102,6 @@ describe PassengersController do
   end
 
   describe "create" do
-=======
->>>>>>> c137cf969c27aa2937c551703ea82638468acc94
     it "will save a new passenger and redirect if valid" do
       passenger_name = "Faiza Ahsan"
       passenger_phone_num = "(123)456-7890"
