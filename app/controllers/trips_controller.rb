2346408ec190bc135f202d4e78bef53d4996b0fc
amyesh <amyesh08@gmail.com>
2019-04-18 18:03:40 -0700

    added edit method and forms for trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,12 +3,11 @@ class TripsController < ApplicationController
     if params[:driver_id]
       @driver = Driver.find_by(id: params[:driver_id])
       @driver_trips = @driver.trips
-
     elsif params[:passenger_id]
       @passenger = Passenger.find_by(id: params[:passenger_id])
       @passenger_trips = @passenger.trips
     else
-      @trips = Trip.all.order(:id) 
+      @trips = Trip.all.order(:id)
     end
   end
 
@@ -21,12 +20,28 @@ class TripsController < ApplicationController
     end
   end
 
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+  end
+
+  def update
+    trip = Trip.find_by(id: params[:id])
+
+    is_successful = trip.update(trip_params)
+
+    if is_successful
+      redirect_to trip_path(trip.id)
+    else
+      @trip = trip
+      render :edit, status: :bad_request
+    end
+  end
+
   def create
     # find available driver
     # create trip with available driver
     # change status of available driver
   end
-
 
   private
 
--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -47,16 +47,3 @@ describe TripsController do
     # Your tests go here
   end
 end
-
-# def index
-#   if params[:driver_id]
-#     @driver = Driver.find_by(id: params[:driver_id])
-#     @driver_trips = @driver.trips
-
-#   elsif params[:passenger_id]
-#     @passenger = Passenger.find_by(id: params[:passenger_id])
-#     @passenger_trips = @passenger.trips
-#   else
-#     @trips = Trip.all.order(:id)
-#   end
-# end
