b4242fe31405160089f92f1ffbc3026c02e22f83
amyesh <amyesh08@gmail.com>
2019-04-18 18:32:09 -0700

    added test for update method in trip controller

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -42,7 +42,66 @@ describe TripsController do
   end
 
   describe "update" do
-    # Your tests go here
+    it "will update an existing trip" do
+      driver = Driver.create(name: "Amy Martinsen", vin: "ABCDEFG")
+      passenger = Passenger.create(name: "Faiza Ahsan", phone_num: "555-555-5555")
+      trip_to_update = Trip.create(passenger_id: passenger.id, driver_id: driver.id, rating: 5, cost: 1474, date: "2016-06-02")
+
+      input_rating = 3
+      input_cost = 1602
+      test_input = {
+        "trip": {
+          rating: input_rating,
+          cost: input_cost,
+          passenger_id: passenger.id,
+          driver_id: driver.id,
+          date: "2016-06-02",
+        },
+      }
+
+      expect {
+        patch trip_path(trip_to_update.id), params: test_input
+      }.wont_change "Trip.count"
+
+      must_respond_with :redirect
+      trip_to_update.reload
+      expect(trip_to_update.rating).must_equal test_input[:trip][:rating]
+      expect(trip_to_update.cost).must_equal test_input[:trip][:cost]
+    end
+
+    it "will return a bad_request (400) when asked to update with invalid data" do
+      driver = Driver.create(name: "Amy Martinsen", vin: "ABCDEFG")
+      passenger = Passenger.create(name: "Faiza Ahsan", phone_num: "555-555-5555")
+      starter_input = {
+        rating: 5,
+        cost: 1602,
+        passenger_id: passenger.id,
+        driver_id: driver.id,
+        date: "2016-06-02",
+      }
+      trip_to_update = Trip.create(starter_input)
+
+      test_input = {
+        "trip": {
+          rating: 3,
+          cost: 1602,
+          passenger_id: nil,
+          driver_id: driver.id,
+          date: "2016-06-02",
+        },
+      }
+
+      # Act
+      expect {
+        patch trip_path(trip_to_update.id), params: test_input
+      }.wont_change "Trip.count"
+
+      # Assert
+      must_respond_with :bad_request
+      trip_to_update.reload
+      expect(trip_to_update.passenger_id).must_equal starter_input[:passenger_id]
+      expect(trip_to_update.rating).must_equal starter_input[:rating]
+    end
   end
 
   describe "create" do
@@ -53,61 +112,3 @@ describe TripsController do
     # Your tests go here
   end
 end
-
-# describe "update" do
-#   it "will update an existing passenger" do
-#     starter_input = {
-#       name: "Faiza Ahsad",
-#       phone_num: "555-555-5555",
-#     }
-
-#     passenger_to_update = Passenger.create(starter_input)
-
-#     input_name = "Amy Martinsen"
-#     input_phone_num = "222-222-2222"
-#     test_input = {
-#       "passenger": {
-#         name: input_name,
-#         phone_num: input_phone_num,
-#       },
-#     }
-
-#     expect {
-#       patch passenger_path(passenger_to_update.id), params: test_input
-#     }.wont_change "Passenger.count"
-
-#     must_respond_with :redirect
-#     passenger_to_update.reload
-#     expect(passenger_to_update.name).must_equal test_input[:passenger][:name]
-#     expect(passenger_to_update.phone_num).must_equal test_input[:passenger][:phone_num]
-#   end
-
-#   it "will return a bad_request (400) when asked to update with invalid data" do
-#     starter_input = {
-#       name: "Punchinello Funny Fellow",
-#       phone_num: "867-5309",
-#     }
-
-#     passenger_to_update = Passenger.create(starter_input)
-
-#     input_name = ""
-#     input_phone_num = "123-456-7890"
-#     test_input = {
-#       "passenger": {
-#         name: input_name,
-#         phone_num: input_phone_num,
-#       },
-#     }
-
-#     # Act
-#     expect {
-#       patch passenger_path(passenger_to_update.id), params: test_input
-#     }.wont_change "Passenger.count"
-
-#     # Assert
-#     must_respond_with :bad_request
-#     passenger_to_update.reload
-#     expect(passenger_to_update.name).must_equal starter_input[:name]
-#     expect(passenger_to_update.phone_num).must_equal starter_input[:phone_num]
-#   end
-# end
